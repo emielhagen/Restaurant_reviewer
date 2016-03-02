972bab81379ebecb455a99a67d6b4186ca4adc9d
emiel hagen <emiel.hagen@gmail.com>
2016-03-02 18:43:15 +0100

    Add restaurant show

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,8 +7,9 @@ class ReviewsController < ApplicationController
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
+    @review.restaurant = @restaurant
     @review.save!
-    redirect_to restaurant_reviews_path(@review)
+    redirect_to restaurant_path(@restaurant)
   end
 
 private
